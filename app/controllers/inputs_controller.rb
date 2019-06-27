c865d7b686bcf8ee8245f114c0678473187ea656
igor araujo <igor.araujo@fulllab.com.br>
2019-06-27 20:48:52 -0300

    installed mail gem to handle .eml files

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -1,44 +1,37 @@
 class InputsController < ApplicationController
   before_action :set_input, only: [:show, :edit, :update, :destroy]
 
-  # GET /inputs
-  # GET /inputs.json
   def index
     @inputs = Input.all
   end
 
-  # GET /inputs/1
-  # GET /inputs/1.json
   def show
   end
 
-  # GET /inputs/new
   def new
     @input = Input.new
   end
 
-  # GET /inputs/1/edit
   def edit
   end
 
-  # POST /inputs
-  # POST /inputs.json
   def create
-    @input = Input.new(input_params)
+    binding.pry
 
-    respond_to do |format|
-      if @input.save
-        format.html { redirect_to @input, notice: 'Input was successfully created.' }
-        format.json { render :show, status: :created, location: @input }
-      else
-        format.html { render :new }
-        format.json { render json: @input.errors, status: :unprocessable_entity }
-      end
-    end
+    mail = Mail.read(params[:file])
+    # @input = Input.new(input_params)
+    #
+    # respond_to do |format|
+    #   if @input.save
+    #     format.html { redirect_to @input, notice: 'Input was successfully created.' }
+    #     format.json { render :show, status: :created, location: @input }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @input.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
-  # PATCH/PUT /inputs/1
-  # PATCH/PUT /inputs/1.json
   def update
     respond_to do |format|
       if @input.update(input_params)
@@ -51,8 +44,6 @@ class InputsController < ApplicationController
     end
   end
 
-  # DELETE /inputs/1
-  # DELETE /inputs/1.json
   def destroy
     @input.destroy
     respond_to do |format|
@@ -62,12 +53,12 @@ class InputsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_input
       @input = Input.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def input_params
       params.require(:input).permit(:name, :phone, :message, :vehicle, :price, :link, :vehicle_brand, :vehicle_model, :km, :accessories)
     end
